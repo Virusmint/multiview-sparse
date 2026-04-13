f7147759e2ef062b50839d7f4f6baf31efe884b0
Virusmint <davidz1015@hotmail.com>
2026-04-12 23:41:51 -0400

    normalized negative l2 similarity

--- src/utils/sim_metric.py
+++ src/utils/sim_metric.py
@@ -2,12 +2,11 @@ import torch
 import torch.nn.functional as F
 
 
-def dot_product(z1: torch.Tensor, z2: torch.Tensor) -> torch.Tensor:
-    return torch.mm(z1, z2.T)
-
-
-def neg_l2_dist(z1: torch.Tensor, z2: torch.Tensor) -> torch.Tensor:
-    return -torch.cdist(z1, z2)
+def normalized_neg_l2_sim(z1: torch.Tensor, z2: torch.Tensor) -> torch.Tensor:
+    z1 = F.normalize(z1, p=2, dim=1)
+    z2 = F.normalize(z2, p=2, dim=1)
+    dist_sq = 2 - 2 * torch.mm(z1, z2.T)
+    return -dist_sq
 
 
 def cosine_sim(z1: torch.Tensor, z2: torch.Tensor) -> torch.Tensor:
